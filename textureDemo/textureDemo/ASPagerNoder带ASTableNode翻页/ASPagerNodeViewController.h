fdaa8c09b24762970044357c66ce08d13bdb75c3
zhouweijie <zhouweijie@jiemian.com>
2018-04-02 16:55:55 +0800

    实现了ASCollectionNode拖动排序，和简单ASPagerNode翻页

--- textureDemo/textureDemo/ASPagerNoder带ASTableNode翻页/ASPagerNodeViewController.h
+++ textureDemo/textureDemo/ASPagerNoder带ASTableNode翻页/ASPagerNodeViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ASPagerNodeViewController : UIViewController
+@interface ASPagerNodeViewController : ASViewController
 
 @end
--- textureDemo/textureDemo/ASPagerNoder带ASTableNode翻页/ASPagerNodeViewController.m
+++ textureDemo/textureDemo/ASPagerNoder带ASTableNode翻页/ASPagerNodeViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ASPagerNodeViewController.h"
+#import "ItemNode.h"
 
-@interface ASPagerNodeViewController ()//<ASPagerDelegate,ASPagerDataSource>
+@interface ASPagerNodeViewController ()<ASPagerDelegate,ASPagerDataSource>
 
 @end
 
@@ -16,15 +17,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    ASPagerNode *pager  = [[ASPagerNode alloc]init];
+    ASPagerNode *pager  = [[ASPagerNode alloc]init];
 //    pager.delegate = self;
-//    pager.dataSource = self;
-//    pager.frame = self.view.bounds;
-//    [self.view addSubnode:pager];
+    pager.dataSource = self;
+    pager.frame = self.view.bounds;
+    [self.view addSubnode:pager];
 }
 #pragma mark - datasource
-//- (NSInteger)numberOfPagesInPagerNode:(ASPagerNode *)pagerNode{
-//    return 20;
-//}
-
+- (NSInteger)numberOfPagesInPagerNode:(ASPagerNode *)pagerNode{
+    return 20;
+}
+- (ASCellNodeBlock)pagerNode:(ASPagerNode *)pagerNode nodeBlockAtIndex:(NSInteger)index{
+    return ^{
+        ItemNode *node = [[ItemNode alloc]initWithString:[NSString stringWithFormat:@"index:%lu",index]];
+        node.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1.0];
+        return node ;
+    };
+}
 @end
